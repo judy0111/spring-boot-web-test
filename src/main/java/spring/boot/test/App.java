a4e41ed6815ca718b152021f10f953aec0efa656
judy0111 <292546459@qq.com>
2017-06-10 13:35:13 +0800

    springboot-demo

---
+++ src/main/java/spring/boot/test/App.java
@@ -0,0 +1,23 @@
+package spring.boot.test;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App
+{
+	@RequestMapping("/mytes")
+	public String sayHello(){
+		return "hello bbworld!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+	
+}
